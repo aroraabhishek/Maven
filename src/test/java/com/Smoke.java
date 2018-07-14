8151ee4ba543b2fd1e8b5882a6a4c4f7f543d916
Abhishek <abhishek.arora22@gmail.com>
2018-07-14 18:08:49 +0530

    commit

---
+++ src/test/java/com/Smoke.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Smoke {
+	
+	@Test
+	public void add(){
+		AssertJUnit.assertEquals(4, 4);
+	}
+
+}
